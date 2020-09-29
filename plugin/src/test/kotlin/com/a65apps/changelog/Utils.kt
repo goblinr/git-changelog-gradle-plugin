b13fd9a7a208f6ad8fd8382c322eea425eee9855
Roman Romanov <rromanov@65gb.net>
2020-09-29 21:22:22 +0400

    Feature: code coverage

--- plugin/src/test/kotlin/com/a65apps/changelog/Utils.kt
+++ plugin/src/test/kotlin/com/a65apps/changelog/Utils.kt
@@ -1,8 +1,11 @@
 package com.a65apps.changelog
 
 import org.gradle.internal.impldep.org.junit.rules.TemporaryFolder
+import org.gradle.testkit.runner.GradleRunner
 import org.spekframework.spek2.dsl.Root
 import org.spekframework.spek2.lifecycle.CachingMode
+import java.io.File
+import java.io.InputStream
 
 fun Root.temporaryFolder() {
     val temporaryFolder by memoized(mode = CachingMode.EACH_GROUP) { TemporaryFolder() }
@@ -13,5 +16,17 @@ fun Root.temporaryFolder() {
 
     afterEachGroup {
         temporaryFolder.delete()
+    }
+}
+
+fun GradleRunner.withJaCoCo(): GradleRunner {
+    javaClass.classLoader.getResourceAsStream("testkit-gradle.properties")
+            ?.toFile(File(projectDir, "gradle.properties"))
+    return this
+}
+
+private fun InputStream.toFile(file: File) {
+    use { input ->
+        file.outputStream().use { input.copyTo(it) }
     }
 }
